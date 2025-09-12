ef56dbe7ef5714118505f1d3006113749cb1fec2
dipesh-debug <dipeshhud@gmail.com>
2025-09-12 17:45:20 +0545

    Fix backend database initialization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Import your components
-import Dashboard from './components/Dashboard';
+
 import ItemDetails from './components/ItemDetails';
 
 function App() {
